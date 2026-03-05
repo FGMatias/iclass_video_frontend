dcb79a33a7af2130288e782b9fc249ea8b1d85bb
FGMatias <matesgon828@gmail.com>
2026-03-04 21:13:05 -0500

    chore(config): setup vite config and axios instance

---
+++ src/lib/axios.ts
@@ -0,0 +1,38 @@
+import { useAuthStore } from '@/stores/auth.store'
+import axios from 'axios'
+
+const api = axios.create({
+  baseURL: '/api',
+  timeout: 15000,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+})
+
+api.interceptors.request.use(
+  (config) => {
+    const token = useAuthStore.getState().token
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`
+    }
+
+    return config
+  },
+  (error) => Promise.reject(error),
+)
+
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      const { logout } = useAuthStore.getState()
+      logout()
+      window.location.href = '/login'
+    }
+
+    return Promise.reject(error)
+  },
+)
+
+export default api
